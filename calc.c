6eedb3374fdd73be46cdbf8b469c143cd3fb7dcc
Носова Мария Сергеевна <ip614s14@awp02.csc.local>
2017-02-20 08:49:42 +0700

    Implement main logic

--- calc.c
+++ calc.c
@@ -15,9 +15,42 @@ int main(void) {
     printf("Enter investment amount: ");
     scanf("%f", &investment);
 
-      if (investment < 0.0) {
+    if (investment < 0.0) {
         printf("Invalid  input, exit");
         return 1;
     }
+
+    if (duration >= 0 && duration <= 30) {
+        investment *= 0.9;
+    }
+
+    if (investment < 100000) {
+        if (duration >= 31 && duration <= 120) {
+            investment *= 1.02;
+        }
+
+        if (duration >= 121 && duration <= 240) {
+            investment *= 1.06;
+        }
+        if (duration >= 241 && duration <= 365) {
+            investment *= 1.12;
+        }
+    }
+
+    if (investment > 100000) {
+        if (duration >= 31 && duration <= 120) {
+            investment *= 1.03;
+        }
+
+        if (duration >= 121 && duration <= 240) {
+            investment *= 1.08;
+        }
+        if (duration >= 241 && duration <= 365) {
+            investment *= 1.15;
+        }
+    }
+
+    printf("Duration time: %d \nInvestment amount: %f", duration, investment);
+    printf("\n");
     return 0;
 }
