db64974ee5bf0c7ece62cf70006b7904f25f9594
Носова Мария Сергеевна <ip614s14@awp02.csc.local>
2017-02-20 08:42:49 +0700

    Implement input validation

--- calc.c
+++ calc.c
@@ -6,7 +6,18 @@ int main(void) {
 
     printf("Enter investment duration: ");
     scanf("%d", &duration);
+    
+    if (duration < 0 || duration > 365) {
+        printf("Invalid input, exit");
+        return 1;
+    }
 
     printf("Enter investment amount: ");
     scanf("%f", &investment);
+
+      if (investment < 0.0) {
+        printf("Invalid  input, exit");
+        return 1;
+    }
+    return 0;
 }
