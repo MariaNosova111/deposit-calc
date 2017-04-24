26f58e1386395c3271547736d88ce529557a2e8e
Носова Мария Сергеевна <ip614s14@awp01.csc.local>
2017-04-24 09:20:37 +0700

    Test_1 created

--- test/validation_test.c
+++ test/validation_test.c
@@ -1,3 +1,14 @@
 #include "deposit.h"
 
 #include "ctest.h"
+
+CTEST(input_tests, amount_more_zero)
+{
+  
+    const int result = inputCheck(100, 1000);
+
+    // Then
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
