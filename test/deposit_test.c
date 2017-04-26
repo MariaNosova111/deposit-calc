1884caba9766de3ec86277b719d8be627bd4be9d
Носова Мария Сергеевна <ip614s14@mwp03.csc.local>
2017-04-26 14:22:28 +0700

    Add more tests

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -1,3 +1,24 @@
 #include "deposit.h"
 
 #include "ctest.h"
+
+CTEST(deposit_tests, amount_less_100000)
+{
+  
+    const int result = perform(40, 5000);
+
+    // Then
+    const int expected = 5100;
+    ASSERT_EQUAL(expected, result);
+}
+CTEST(deposit_tests, amount_more_100000)
+{
+  
+    const int result = perform(40, 500000);
+
+    // Then
+    const int expected = 515000;
+    ASSERT_EQUAL(expected, result);
+}
+
+
--- test/validation_test.c
+++ test/validation_test.c
@@ -2,6 +2,24 @@
 
 #include "ctest.h"
 
+CTEST(input_tests, duration_less_365)
+{
+  
+    const int result = inputCheck(100, 1000);
+
+    // Then
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+CTEST(input_tests, duration_more_zero)
+{
+  
+    const int result = inputCheck(100, 1000);
+
+    // Then
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
 CTEST(input_tests, amount_more_zero)
 {
   
